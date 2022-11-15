7fc5d448fbf309b167394b252384eba099054ed5
juan-zv <juanzuritavasquez@gmail.com>
2022-11-15 13:44:48 -0700

    added option to change profile picture

--- screens/Profile.js
+++ screens/Profile.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useRef } from 'react';
-import { StyleSheet, Text, View, Image, SafeAreaView , Share, ScrollView, Button} from 'react-native';
+import { StyleSheet, Text, View, Image, SafeAreaView , Share, ScrollView, Button, TouchableOpacity} from 'react-native';
 import { Card, CardTitle, CardContent} from 'react-native-material-cards';
 import BarChart from 'react-native-bar-chart';
 import AsyncStorage from '@react-native-async-storage/async-storage';
@@ -7,21 +7,29 @@ import { Camera } from "expo-camera";
 // import Share from 'react-native-share';
 
 
+const cameraOptions={
+  quality:0,
+  exif:false
+}
 
 
 
 const Profile = (props) => {
   const [userName, setUserName] = useState("");
   const [cameraPermission, setCameraPermission] = useState(false);
-  const [profilephoto, setProfilePhoto] = useState(null);
+  const [profilePhoto, setProfilePhoto] = useState(null);
   const cameraRef = useRef(null);
+  const [cameraReady, setCameraReady] = useState(false);
 
   useEffect(()=>{
     const getUserName = async ()=>{
       const cameraPermission = await Camera.requestCameraPermissionsAsync();
       setCameraPermission(cameraPermission);
-      const userName = AsyncStorage.getItem("userName");
+      const userName = await AsyncStorage.getItem("userName");
       setUserName(userName);
+      await AsyncStorage.removeItem("profilePhoto")
+      const profilePhoto = await AsyncStorage.getItem("profilePhoto");
+      setProfilePhoto(profilePhoto);
     };
     getUserName();
   },[]);
@@ -38,7 +46,26 @@ const Profile = (props) => {
   console.log('Error', error)
       }
     }
+    if(profilePhoto==null){
+      return(
+        <View style={styles.container}>
+          <Camera style={styles.camera} ref={cameraRef} onCameraReady={()=>{setCameraReady(true)}}>
+            <View style={styles.buttonContainer}>
+              {cameraReady?<TouchableOpacity style={styles.button} onPress={async ()=> {
 
+                const picture = await cameraRef.current.takePictureAsync(cameraOptions);
+                console.log("Picture", picture);
+                await AsyncStorage.setItem("profilePhoto", picture.uri);
+                setProfilePhoto(picture.uri);
+              }}>
+                <Text style={styles.text}>Take Picture</Text>
+              </TouchableOpacity>: null}
+            </View>
+          </Camera>
+        </View>
+      )
+    }
+    else{
   return (
     <SafeAreaView style={{flex: 1}}>
          <Card style={{backgroundColor:'white', borderRadius: 10, margin:20 ,width: 320, shadowColor: "#000",
@@ -52,7 +79,7 @@ shadowRadius: 2.62,
 elevation: 4}}>
      <CardContent>
      <Image style={{height: 100, width:100, borderRadius: 75}}
-      source={require('../image/me.jpg')} />
+      source={{uri:profilePhoto}} />
     <Text style={{marginTop:10,marginBottom:10,fontWeight: 'bold'}}>{userName}</Text>
 
     <Text style={{marginTop:20,marginBottom:2}}>This Week's progress</Text>
@@ -64,11 +91,31 @@ elevation: 4}}>
     </Card>
  </SafeAreaView>
   );
-};
+}};
 export default Profile;
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    padding:20
-  }
-})
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 20
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: 'transparent',
+    margin: 64,
+  },
+  button: {
+    flex: 1,
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+});
